05701bc9db6e69e51ec2050dda6ac366c7e353bc
yashmaya <yashmaya91@gmail.com>
2026-01-17 13:05:58 +0400

    2nd task

---
+++ 3rd task.py
@@ -0,0 +1,5 @@
+length = float(input("Enter length of rectangle"))
+width = float(input("Enter width of rectangle"))
+perimetr = 2 * (length + width)
+area = length * width
+print("area of rectangle is" , area)
