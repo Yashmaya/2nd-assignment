796acbdb31cc420e63155e262e249b60f10dd097
yashmaya <yashmaya91@gmail.com>
2026-01-17 18:21:24 +0400

    4th task

---
+++ 4th task.py
@@ -0,0 +1,10 @@
+first_num = float(input("enter first integer"))
+second_num = float(input("enter second integer"))
+third_number = float(input("enter third integar"))
+sum = first_num + second_num + third_number
+print("sum is:" , sum)
+print("product is", 'product')
+print("average is", 'average')
+
+
+
